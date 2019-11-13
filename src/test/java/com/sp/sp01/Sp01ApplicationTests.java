8133b7eb860efae92dc2172cf6f8c948bad2279a
a1015493089 <1015493089@qq.com>
2019-11-13 22:33:54 +0800

    t02-1

---
+++ src/test/java/com/sp/sp01/Sp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.sp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
